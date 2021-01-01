8d63e204d493a56aea118251b6a4bc79ab197179
Pham Viet Hoa <phamviethoait@gmail.com>
2021-01-01 11:25:29 +0700

    REVISED CATEGORY MANAGEMENTS FEATURE OF ADMIN
      - Modify add category level feature.
      - Update add category feature.
      - Update update category featture.
      - Modify a new level name column in categories table.

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -2,6 +2,7 @@ import Category from "../models/categories.model.js";
 import Course from "../models/courses.model.js";
 import Student from "../models/students.model.js";
 import Teacher from "../models/teachers.model.js";
+import CategoryLevel from "../models/categoryLevels.model.js";
 import bcrypt from "bcryptjs";
 
 export const handle_admin_get = (req, res) => {
@@ -12,19 +13,19 @@ export const handle_admin_get = (req, res) => {
 };
 
 export const handle_categories_get = async (req, res) => {
-  const categories = await Category.find({}, "categoryName");
-  const count = await Course.count({ category: "Web" });
+  const categories = await Category.find({}, "categoryName categoryLevel");
 
   const categoriesHBS = [];
-
   for (let i = 0; i < categories.length; i++) {
     const name = categories[i].categoryName;
     const order = i + 1;
     const numberOfCourse = await Course.count({ category: name });
+    const levelName = categories[i].categoryLevel;
 
     categoriesHBS.push({
       name,
       order,
+      levelName,
       numberOfCourse: +numberOfCourse,
     });
   }
@@ -33,6 +34,7 @@ export const handle_categories_get = async (req, res) => {
     layout: "admin.hbs",
     title: "Project | Categories",
     categories: categoriesHBS,
+    //categorylevelName: categoryLevelsHBS,
   });
 };
 
@@ -83,10 +85,16 @@ export const handle_delete_course_post = async (req, res) => {
   res.redirect("/admin/courses");
 };
 
-export const handle_add_category_get = (req, res) => {
+export const handle_add_category_get = async (req, res) => {
+  const categoryLevels = await CategoryLevel.find({}, "levelName");
+  const levelName = categoryLevels.map((categoryLevel) => {
+    return categoryLevel.levelName;
+  });
+
   res.render("vwAdmin/addCategory", {
     layout: "admin.hbs",
     title: "Project | Create Category",
+    levelName,
   });
 };
 
@@ -100,10 +108,21 @@ export const handle_is_available_category_name = async (req, res) => {
   res.json({ isExistCategoryName });
 };
 
+export const handle_is_available_category_level = async (req, res) => {
+  const levelName = req.query.levelName;
+
+  const isExistLevelName = await CategoryLevel.exists({
+    levelName: levelName,
+  });
+
+  res.json({ isExistLevelName });
+};
+
 export const handle_add_category_post = async (req, res) => {
-  const { categoryName, categoryDetail } = req.body;
+  const { categoryName, categoryDetail, levelName } = req.body;
 
   const newCategory = new Category({
+    categoryLevel: levelName,
     categoryName: categoryName,
     categoryDetail: categoryDetail,
   });
@@ -128,20 +147,26 @@ export const handle_update_category_get = async (req, res) => {
     "categoryName categoryDetail"
   );
 
+  const categoryLevels = await CategoryLevel.find({}, "levelName");
+  const levelName = categoryLevels.map((categoryLevel) => {
+    return categoryLevel.levelName;
+  });
+
   res.render("vwAdmin/updateCategory", {
     layout: "admin.hbs",
     title: "Project | Update Category",
     name: category.categoryName,
     detail: category.categoryDetail,
+    levelName,
   });
 };
 
 export const handle_update_category_post = async (req, res) => {
-  const { categoryName, categoryDetail } = req.body;
+  const { categoryName, categoryDetail, levelName } = req.body;
 
   await Category.updateOne(
     { categoryName: categoryName },
-    { categoryDetail: categoryDetail }
+    { categoryDetail: categoryDetail, categoryLevel: levelName }
   );
 
   res.redirect("/admin/categories");
@@ -279,4 +304,23 @@ export const handle_add_teacher_post = async (req, res) => {
 
   const url = "/admin/teachers";
   res.redirect(url);
+};
+
+export const handle_add_category_level_get = (req, res) => {
+  res.render("vwAdmin/addCategoryLevel", {
+    title: "Project | Category levels",
+    layout: "admin.hbs",
+  });
+};
+
+export const handle_add_category_level_post = async (req, res) => {
+  const levelName = req.body.levelName;
+
+  const newCategorylevel = new CategoryLevel({
+    levelName,
+  });
+
+  await newCategorylevel.save();
+
+  res.redirect(`/admin/categories`);
 };
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -3,89 +3,82 @@ import bodyParser from "body-parser";
 const router = express.Router();
 const urlencodedParser = bodyParser.urlencoded({ extended: true });
 
-import {
-  handle_admin_get,
-  handle_categories_get,
-  handle_courses_get,
-  handle_students_get,
-  handle_teachers_get,
-  handle_add_category_get,
-  handle_add_category_post,
-  handle_delete_category_post,
-  handle_update_category_get,
-  handle_update_category_post,
-  handle_is_available_category_name,
-  handle_delete_course_post,
-  handle_delete_student_post,
-  handle_detail_course_get,
-  handle_detail_student_get,
-  handle_delete_teacher_post,
-  handle_detail_teacher_get,
-  handle_add_teacher_get,
-  handle_add_teacher_post,
-  handle_is_available_teacher,
-} from "../controllers/admin.controller.js";
+import * as admin from "../controllers/admin.controller.js";
 
-router.route("/").get(handle_admin_get);
+router.route("/").get(admin.handle_admin_get);
 
-router.route("/categories").get(handle_categories_get);
+router.route("/categories").get(admin.handle_categories_get);
 
-router.route("/categories/add").get(handle_add_category_get);
+router.route("/categories/add").get(admin.handle_add_category_get);
 
-router.route("/categories/is-available").get(handle_is_available_category_name);
+router
+  .route("/categories/is-available")
+  .get(admin.handle_is_available_category_name);
+
+router.route("/category-levels/add").get(admin.handle_add_category_level_get);
+
+router
+  .route("/category-levels/add")
+  .post(urlencodedParser, admin.handle_add_category_level_post);
+
+router
+  .route("/category-levels/is-available")
+  .get(admin.handle_is_available_category_level);
 
 router
   .route("/categories/add")
-  .post(urlencodedParser, handle_add_category_post);
+  .post(urlencodedParser, admin.handle_add_category_post);
 
 router
   .route("/categories/delete")
-  .post(urlencodedParser, handle_delete_category_post);
+  .post(urlencodedParser, admin.handle_delete_category_post);
 
 router
   .route("/categories/update")
-  .get(urlencodedParser, handle_update_category_get);
+  .get(urlencodedParser, admin.handle_update_category_get);
 
 router
   .route("/categories/update")
-  .post(urlencodedParser, handle_update_category_post);
+  .post(urlencodedParser, admin.handle_update_category_post);
 
-router.route("/courses").get(handle_courses_get);
+router.route("/courses").get(admin.handle_courses_get);
 
 router
   .route("/courses/detail")
-  .post(urlencodedParser, handle_detail_course_get);
+  .post(urlencodedParser, admin.handle_detail_course_get);
 
 router
   .route("/courses/delete")
-  .post(urlencodedParser, handle_delete_course_post);
+  .post(urlencodedParser, admin.handle_delete_course_post);
 
-router.route("/students").get(handle_students_get);
+router.route("/students").get(admin.handle_students_get);
 
 router
   .route("/students/detail")
-  .post(urlencodedParser, handle_detail_student_get);
+  .post(urlencodedParser, admin.handle_detail_student_get);
 
 router
   .route("/students/delete")
-  .post(urlencodedParser, handle_delete_student_post);
+  .post(urlencodedParser, admin.handle_delete_student_post);
 
-router.route("/teachers").get(handle_teachers_get);
+router.route("/teachers").get(admin.handle_teachers_get);
 
 router
   .route("/teachers/delete")
-  .post(urlencodedParser, handle_delete_teacher_post);
+  .post(urlencodedParser, admin.handle_delete_teacher_post);
 
 router
   .route("/teachers/detail")
-  .post(urlencodedParser, handle_detail_teacher_get);
+  .post(urlencodedParser, admin.handle_detail_teacher_get);
 
-router.route("/teachers/add").get(handle_add_teacher_get);
+router.route("/teachers/add").get(admin.handle_add_teacher_get);
 
 router
   .route("/teachers/is-available")
-  .get(urlencodedParser, handle_is_available_teacher);
+  .get(urlencodedParser, admin.handle_is_available_teacher);
 
-router.route("/teachers/add").post(urlencodedParser, handle_add_teacher_post);
+router
+  .route("/teachers/add")
+  .post(urlencodedParser, admin.handle_add_teacher_post);
 
 export default router;

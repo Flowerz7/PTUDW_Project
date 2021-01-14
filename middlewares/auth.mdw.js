176ab8c772011ab3fc1504ccf0cfbf1e7d05762b
Pham Viet Hoa <phamviethoait@gmail.com>
2021-01-14 09:51:57 +0700

    fix auth student

--- middlewares/auth.mdw.js
+++ middlewares/auth.mdw.js
@@ -1,3 +1,13 @@
+export const studentAuth = (req, res, next) => {
+  if (req.session.isAuth === true) {
+    if (req.session.role === "admin" || req.session.role === "teacher") {
+      return res.redirect("account/login");
+    }
+  }
+
+  next();
+};
+
 export const adminAuth = (req, res, next) => {
   if (req.session.isAuth === false || req.session.role !== "admin") {
     return res.redirect("/account/login");
--- middlewares/route.mdw.js
+++ middlewares/route.mdw.js
@@ -3,8 +3,8 @@ import adminRouter from "../routes/admin.route.js";
 import homeRouter from "../routes/front/home.route.js";
 import courseRouter from "../routes/courses.js";
 import teacherRouter from "../routes/front/teacher.route.js";
-import categoryController from '../routes/categories.js'
-import studentsController from '../routes/students.js'
+import categoryController from "../routes/categories.js";
+import studentsController from "../routes/students.js";
 import * as authPage from "./auth.mdw.js";
 
 const routing = (app) => {
@@ -13,10 +13,10 @@ const routing = (app) => {
   app.use("/account", accountRouter);
   app.use("/courses", courseRouter);
   app.use("/teacher", authPage.teacherAuth, teacherRouter);
-  app.use('/category', categoryController)
-  app.use('/students', studentsController)
+  app.use("/category", categoryController);
+  app.use("/students", studentsController);
 
-  app.use("/", homeRouter);
+  app.use("/", authPage.studentAuth, homeRouter);
 };
 
 export default routing;
